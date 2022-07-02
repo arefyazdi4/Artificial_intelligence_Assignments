be5a4b0cff481205a8d475affb2fd57e0929aee1
RF_BURGER <arefyazdi4@gmail.com>
2022-07-02 17:45:47 +0430

    updating Minmax start func

--- project_3_minimax/branch.py
+++ project_3_minimax/branch.py
@@ -3,7 +3,7 @@ from board import BoardTicTak
 
 class Branch:
     def __init__(self, board: BoardTicTak, action: tuple[int, int], player_state):
-        self.board: BoardTicTak = board.__copy__()
+        self.board = board.__copy__()
         self.board.get_square(action).state = player_state
         self.board.set_board_state()
         self.state = board.state
--- project_3_minimax/minimax.py
+++ project_3_minimax/minimax.py
@@ -1,4 +1,3 @@
-from math import inf
 from branch import Branch
 from board import BoardTicTak
 from square import Square
@@ -6,9 +5,9 @@ from square import Square
 
 class MiniMax:
     def __init__(self):
-        self.game_tree: Branch = Branch(board=BoardTicTak(), mode=MiniMax.maxi, action=(1, 1))
+        pass
 
-    def maxi(self):
+    def maxi(self, branch: Branch) -> tuple[Branch, tuple[int, int]]:
         pass
 
     def mini(self):
@@ -17,10 +16,25 @@ class MiniMax:
     def pruning(self):
         pass
 
-    def start(self, current_branch):
-        pass
+    def start(self):
+        board = BoardTicTak()
+        last_state = board.state
+        print(board)
+        while last_state == BoardTicTak.UNSIGNED_STATE:
+            # player -> O turn to move
+            action_o_str = input('Enter Your Move <ij>: ')
+            action_o = (int(action_o_str[0]), int(action_o_str[1]))  # ij str to tuple (i,j)
+            # apply player move to board and show result
+            branch = Branch(board=board, action=action_o, player_state=Square.PLAYER_O_STATE)
+            print(branch.board)
+
+            # Ai -> X turn to move
+            (branch, action_x) = self.maxi(branch)
+            board = branch.board
+            last_state = branch.state
+            print(branch.board)
 
 
 if __name__ == '__main__':
     game = MiniMax()
-    game.start(game.game_tree)
+    game.start()
