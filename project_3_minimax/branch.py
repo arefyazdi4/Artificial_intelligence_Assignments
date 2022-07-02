27fe3c2c483accb42d9ec431890362d7d8e610de
RF_BURGER <arefyazdi4@gmail.com>
2022-07-02 16:14:05 +0430

    updating branch

--- project_3_minimax/branch.py
+++ project_3_minimax/branch.py
@@ -1,18 +1,8 @@
 from minimax import MiniMax
 from board import BoardTicTak
-from math import inf
 
 
 class Branch:
-    MODE_MAX = MiniMax.maxi
-    MODE_MIN = MiniMax.mini
-
-    def __init__(self, board: BoardTicTak, mode, action: tuple[int, int]):
+    def __init__(self, board: BoardTicTak):
         self.board = board.__copy__()
-        self.mode = mode
-        self.action = action
         self.state = board.state
-        if mode == Branch.MODE_MAX:
-            self.range = [0, inf]
-        else:
-            self.range = [-inf, 0]
