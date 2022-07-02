1cff6522b25228320e82ad816e820e3bc19ae2e3
RF_BURGER <arefyazdi4@gmail.com>
2022-07-02 17:36:12 +0430

    updating branch set an action on square player state

--- project_3_minimax/branch.py
+++ project_3_minimax/branch.py
@@ -1,8 +1,9 @@
-from minimax import MiniMax
 from board import BoardTicTak
 
 
 class Branch:
-    def __init__(self, board: BoardTicTak):
-        self.board = board.__copy__()
+    def __init__(self, board: BoardTicTak, action: tuple[int, int], player_state):
+        self.board: BoardTicTak = board.__copy__()
+        self.board.get_square(action).state = player_state
+        self.board.set_board_state()
         self.state = board.state
