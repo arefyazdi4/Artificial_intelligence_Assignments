e29251809c3c07e960759db4c608eec61e63b740
RF_BURGER <arefyazdi4@gmail.com>
2022-07-02 15:42:19 +0430

    init branch

--- project_3_minimax/minimax.py
+++ project_3_minimax/minimax.py
@@ -1,73 +1,7 @@
 from math import inf
-
-
-class Square:
-    UNSIGNED_STATE = None
-    PLAYER_X_STATE = 'X'
-    PLAYER_O_STATE = 'O'
-
-    def __init__(self, coordinate: tuple[int, int], state: str = UNSIGNED_STATE):
-        self.coordinate = coordinate
-        self.state = state
-
-    def __str__(self):
-        if self.state == Square.UNSIGNED_STATE:
-            return '.'
-        else:
-            return self.state
-
-
-class BoardTicTak:
-    WIN_STATE = 1
-    LOSE_STATE = -1
-    DRAW_STATE = 0
-    UNSIGNED_STATE = None
-
-    def __init__(self):
-        self.state: str = BoardTicTak.UNSIGNED_STATE
-        self.board: list[list[Square]] = [[Square(coordinate=(i, j)) for j in range(3)] for i in range(3)]
-
-    def __copy__(self):
-        return self.board.copy()
-
-    def __getitem__(self, item: tuple[int, int]):  # return square obj with (i,j) coordinate
-        for row in self.board:
-            for square in row:
-                if square.coordinate == item:
-                    return square
-        return None
-
-    def __str__(self):
-        board: str = str()
-        for row in self.board:
-            board += '\n'  # goes for next row
-            for square in row:
-                board += square.__str__() + '  '  # goes for next column
-        return board
-
-    def set_state(self):
-        for row in self.board:
-            for square in row:
-                signed_state = square.state
-                if signed_state == Square.PLAYER_X_STATE:
-                    pass
-                elif signed_state == Square.PLAYER_O_STATE:
-                    pass
-
-
-class Branch:
-    MODE_MAX = MiniMax.maxi
-    MODE_MIN = MiniMax.mini
-
-    def __init__(self, board: BoardTicTak, mode, action:tuple[int,int] ):
-        self.board = board
-        self.mode = mode
-        self.action = action
-        self.state = board.state
-        if mode == Branch.MODE_MAX:
-            self.range = [0, inf]
-        else:
-            self.range = [-inf, 0]
+from branch import Branch
+from board import BoardTicTak
+from square import Square
 
 
 class MiniMax:
@@ -84,7 +18,7 @@ class MiniMax:
         pass
 
     def start(self, current_branch):
-
+        pass
 
 
 if __name__ == '__main__':
