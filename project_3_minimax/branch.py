119516f936d74d3a5d34f47c5cba847cf23d5585
RF_BURGER <arefyazdi4@gmail.com>
2022-07-04 18:15:09 +0430

    Commenting board.py

--- project_3_minimax/branch.py
+++ project_3_minimax/branch.py
@@ -1,12 +1,17 @@
 from board import BoardTicTak
 
 
+# each branch hold a info around their
+# uniq game board specific action than have been made an player that made that action
 class Branch:
     def __init__(self, board: BoardTicTak, action: tuple[int, int], player_state):
-        self.board = board.__copy__()
-        square = self.board.get_square(action)
-        square.state = player_state
-        self.board.set_board_state()
-        self.state = self.board.state
         self.action = action
         self.player_state = player_state
+        # creating a new board so that changes doesn't be effected of others
+        self.board = board.__copy__()
+        # made move on new board
+        self.board.get_square(action).state = player_state
+        # check a new board state
+        self.board.set_board_state()
+        # set a branch state a what we get from current board
+        self.state = self.board.state
